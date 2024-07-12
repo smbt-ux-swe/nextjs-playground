7c751fa6c850a27c6f762c529717d31b09995486
Sukmin Hwang <dev.suuk@gmail.com>
2024-07-11 23:56:48 -0500

    Wrap li in ul

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,12 +4,14 @@ export default function ProductsPage() {
   return (
     <>
       <h1> products list </h1>
-      <Link href="/products/scarf">
-        <li>scarf</li>
-      </Link>
-      <Link href="/products/necklass">
-        <li>necklass</li>
-      </Link>
+      <ul>
+        <Link href="/products/scarf">
+          <li>scarf</li>
+        </Link>
+        <Link href="/products/necklass">
+          <li>necklass</li>
+        </Link>
+      </ul>
     </>
   );
 }
